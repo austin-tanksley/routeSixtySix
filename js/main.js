4102bc37690baeda710b6f68f0d858f0f363582b
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2024-08-23 12:28:13 -0500

    shjdkj

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var scene = document.querySelector("a-scene");
+    var vid = document.getElementById("video");
+    var videoShere = document.getElementById("videoShere");
+
+    if (scene.hasLoaded) {
+      run();
+    } else {
+      scene.addEventListener("loaded", run);
+    }
+    
+    function run () {
+      if(AFRAME.utils.device.isMobile()) {
+        document.querySelector('#splash').style.display = 'flex';
+        document.querySelector('#splash').addEventListener('click', function () {
+          playVideo();
+          this.style.display = 'none';
+        })
+      } else {
+          playVideo();
+      }
+    }
+    
+    function playVideo () {
+      vid.play();
+      videoShere.components.material.material.map.image.play();
+    }
+  })
